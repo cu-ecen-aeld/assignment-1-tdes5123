7595f9dd3d4597317b7a1ac6f364b99a121bdbbd
tdes5123 <tdes5123@Ubuntu.myguest.virtualbox.org>
2025-08-25 20:22:14 -0600

    fixed scripts

---
+++ finder-app/writer.sh
@@ -0,0 +1,12 @@
+writefile="$1"
+writestr="$2"
+
+if [[ $# -eq 0 || -z "$2" || -z "$1" ]]; then
+	echo "Missing arguments"
+	exit 1
+fi
+
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then
+	echo "Error: File could not be created."
+fi
